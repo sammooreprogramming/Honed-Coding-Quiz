0f6c577e4a931856b43a29b2ce3de9bbeeeb7d38
Sam Moore <sammooresemail23@gmail.com>
2023-05-22 11:05:30 -0700

    frame is done

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,52 @@ const dBtn = document.getElementById("AnswerD-btn");
 const initialsBtn = document.getElementById("Initials-btn");
 const returnBtn = document.getElementById("Return-btn");
 const clearBtn = document.getElementById("Clear-btn");
+const startBtn = document.getElementById("Start-btn");
+// other HTML elements declared as JS variables //
+const headercard = document.getElementById("Header-card");
+const flashcards = document.getElementById("Flashcards-card");
+const startcard = document.getElementById("Start-card");
+const endgamecard = document.getElementById("Endgame-card");
+const leaderboardcard = document.getElementById("Leaderboard-card");
+// time variables //
+const timeleft = 60;
+// score variables //
+const highscores = [];
+// questions declared in an array as a variable //
+const questions = [
+    { question: 'What is javascript?',
+      answer: 'A. a programming language',
+      choices: [{choice: 'A. a programming language '}, {choice: 'B. a virus'}, {choice: 'C. a kind of coffee'}, {choice: 'D. a type of data'}]
+    },
+    { question: 'Inside which HTML element do we put the javascript?', 
+      answer: '', 
+      choices: [{choice: '1. <h1>'}, {choice: '2. <js>'}, {choice: '3. <script>'}, {choice: '4. <head>'}]
+    },
+    { question: 'What is an array?',
+      answer: 'A. a list of items',
+      choices: [{choice: 'A. a list of items'}, {choice: 'B. a type of sea creature'}, {choice: 'C. a type of HTML tag'}, {choice: 'D. a type of data'}]
+    },
+    { question: 'what is a function?', 
+      answer: 'C. a reusable block of code that accomplishes a task', 
+      choices: [{choice: 'A. a robots purpose'}, {choice: 'B. a type of data that uses characters'}, {choice: 'C. a reusable block of code that accomplishes a task'}, {choice: 'D. a sweet fedora'}]
+    },
+    { question: 'What is a variable?',
+        answer: 'D. a container for storing data values',
+        choices: [{choice: 'A. a type of data'}, {choice: 'B. a type of HTML tag'}, {choice: 'C. a type of rare pokemon'}, {choice: 'D. a container for storing data values'}]
+    },
+    ];
+
+  
 
 
 
-// 1. Create a start button
+// code //
 
 
 // 2. Create a timer
 
 
-// 3. Create a question
+// 3. Create a question array
 
 
 // 4. Create a score
@@ -39,13 +75,13 @@ const clearBtn = document.getElementById("Clear-btn");
 
 // 10. Create a view high score page with a list of high scores and a go back button and a clear button and a submit button
 
-// 11. Create a function to start the quiz
+// 11. Create a function to start the questionuiz
 
-// 12. Create a function to end the quiz
+// 12. Create a function to end the questionuiz
 
 // Create cards for each section.
 
-// 1. Start Quiz 
-// 2. Questions 
+// 1. Start questionuiz 
+// 2.
 // 3. Score
 // 4.High Score
