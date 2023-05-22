c32a7215bdd8711fd43c00d441016b1fed47bf6c
Sam Moore <sammooresemail23@gmail.com>
2023-05-22 10:02:14 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// CODING QUIZ //
+// 1. Create a start button
+
+
+// 2. Create a timer
+
+
+// 3. Create a question
+
+
+// 4. Create a score
+
+
+// 5. Create a high score
+
+
+// 6. Create a submit button
+
+
+// 7. Create a clear button
+
+
+// 8. Create a go back button
+
+// 9. Create a view high score button
+
+// 10. Create a view high score page with a list of high scores and a go back button and a clear button and a submit button
+
+// 11. Create a function to start the quiz
+
+// 12. Create a function to end the quiz
+
+// Create cards for each section.
+
+// 1. Start Quiz 
+// 2. Questions 
+// 3. Score
+// 4.High Score
